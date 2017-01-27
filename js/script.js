042c40298d734e47449969823353f56c7708aa8e
UHanczar <u.hanczar@gmail.com>
2017-01-27 22:26:37 +0300

    adding checkingLength function & other enhancings

--- js/script.js
+++ js/script.js
@@ -23,17 +23,60 @@ let val;
 let result = '';
 
 
+function checkNumberLength() {
+  // if(number.length < 8 || result.length < 8) {
+  //   display.style.fontSize = 64 + 'px';
+  // } else
+
+  if(number.length >= 8 && number.length < 10 || result.length >= 8 && result.length < 10) {
+    display.style.fontSize = 54 + 'px';
+  } else if(number.length >= 10 && number.length <= 12 || result.length >= 10 && result.length <= 12) {
+      display.style.fontSize = 44 + 'px';
+    } else if(number.length >= 13 &&  number.length <= 15  || result.length >= 13 && result.length <= 15) {
+      display.style.fontSize = 34 + 'px';
+    }
+    // else if(number.length > 15 || result.length > 15) {
+    //   number = 'Err';
+    //   result = 'Err';
+    //   display.style.fontSize = 64 + 'px';
+    //   number === 'Err' ? display.innerText = number : display.innerText = result;
+    // }
+
+    else if(result.length > 15) {
+      result = 'Err';
+      display.style.fontSize = 64 + 'px';
+      display.innerText = result;
+    }
+    else if(number.length < 8 &&  number.length >= 0 || result.length < 8 && result.length >= 0) {
+    display.style.fontSize = 64 + 'px';
+  }
+}
+
 
 function addNumber(e) {
-  if(display.innerText) {
+  val = e.target.dataset.value;
+
+  if(result !== '') {
+    result = '';
     display.innerText = number;
   }
 
+  if(result === 'Err') {
+    number = '';
+    result = '';
+  }
+
+  if(number.length === 15) {
+    return false;
+  }
+
   if(e.target.classList.contains('number')) {
-    val = e.target.dataset.value;
+    //val = e.target.dataset.value;
     number += val;
-    console.log(number);
+    // console.log(number);
+    checkNumberLength();
     display.innerText = number;
+
   } else {
     return false;
   }
@@ -41,11 +84,23 @@ function addNumber(e) {
 }
 
 function addOperator(e) {
+
+
   val = e.target.dataset.value;
-  operator = val;
-  firstNumber = number;
-  number = '';
-  // console.log(firstNumber, number, operator);
+
+  if(result !== '') {
+    firstNumber = result;
+    result = '';
+    number = '';
+    operator = val;
+    } else {
+    operator = val;
+    firstNumber = number;
+    number = '';
+  }
+
+
+  console.log('fNum:' + firstNumber, 'num:' + number, 'op:' + operator);
 }
 
 function evaluate() {
@@ -66,10 +121,12 @@ function evaluate() {
     result = (parseFloat(firstNumber) / parseFloat(number)).toString(10);
     console.log(result);
   }
+
   display.innerText = result;
+  checkNumberLength();
   firstNumber = '';
   number = '';
-  result = '';
+  // result = '';
 
 
 }
@@ -79,7 +136,7 @@ function clearLNumb() {
 
   number = '';
   display.innerText = '0';
-  console.log(firstNumber, number + 'second console');
+  // console.log(firstNumber, number + 'second console');
 }
 
 function cleanAll() {
@@ -97,8 +154,15 @@ function delLastSymbol(e) {
   addNumber(e);
   // let length = number.length;
   // console.log(number + 'before del');
+
   number = number.substring(0, number.length - 1);
+  display.innerText = number;
+  checkNumberLength();
+  // console.log(number);
   // console.log(number + 'after del');
+  if(number === '') {
+    display.innerText = 0;
+  }
 }
 
 function findSqrt() {
