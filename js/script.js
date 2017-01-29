9c39296c08a400fecf470d6081a952fd25c80008
UHanczar <u.hanczar@gmail.com>
2017-01-30 00:24:14 +0300

    enchansing functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Dom variables
 const display = document.querySelector('.enter-field');
 const evalDiv = document.querySelector('.eval-div');
 const currentResult = document.querySelector('.current-result');
@@ -10,83 +11,78 @@ const deleteLastSymbol = document.querySelector('[data-value="«"]');
 const sqrt = document.getElementById('sqrt');
 const equal = document.getElementById('equal');
 
+
 // a variable, that stores current number
 let number = '0';
+// a variable stores operator;
 let operator = '';
 // a variable, that stores previous number
 let firstNumber = '';
-// display current number on the screen
-currentResult.innerText = '0';
-let val;
+
 // a variable, that stores value after evaluation
 let result = '';
-// a variable for evaluation process div;
-let evaluationProcess = '';
+// display current number on the screen
+currentResult.innerText = '0';
 
 
+// function checks number length to be less than 18 characters  and changes font-size according to number's length
 function checkNumberLength() {
-  // if(number.length < 8 || result.length < 8) {
-  //   display.style.fontSize = 64 + 'px';
-  // } else
 
   if(number.length >= 8 && number.length < 10 || result.length >= 8 && result.length < 10) {
       currentResult.style.fontSize = 54 + 'px';
   } else if(number.length >= 10 && number.length <= 12 || result.length >= 10 && result.length <= 12) {
       currentResult.style.fontSize = 44 + 'px';
-    } else if(number.length >= 13 &&  number.length <= 15  || result.length >= 13 && result.length <= 15) {
-      currentResult.style.fontSize = 34 + 'px';
-    }
-    // else if(number.length > 15 || result.length > 15) {
-    //   number = 'Err';
-    //   result = 'Err';
-    //   display.style.fontSize = 64 + 'px';
-    //   number === 'Err' ? display.innerText = number : display.innerText = result;
-    // }
-
-    else if(result.length > 15) {
+    } else if(number.length >= 13 &&  number.length <= 19  || result.length >= 13 && result.length <= 19) {
+      currentResult.style.fontSize = 30 + 'px';
+    } else if(number.length > 19 || result.length > 19) {
       result = 'Err';
-      currentResult.style.fontSize = 64 + 'px';
       currentResult.innerText = result;
-    }
-    else if(number.length < 8 &&  number.length >= 0 || result.length <  8 && result.length >= 0) {
+      currentResult.style.fontSize = 64 + 'px';
+
+    } else if(number.length < 8 &&  number.length >= 0 || result.length <  8 && result.length >= 0) {
      currentResult.style.fontSize = 64 + 'px';
   }
 }
 
-
+// function adds characters to 'number' varible
 function addNumber(e) {
-  val = e.target.dataset.value;
+  let val = e.target.dataset.value;
+
+  // condition works, when first character of new number adds, and default value changes
   if(number === '0') {
     number = '';
   }
 
-
+  // condition works when new number is adding just after evaluation
   if(result !== '') {
     result = '';
     currentResult.innerText = number;
   }
 
+  // if Error - number & result = 0
   if(result === 'Err') {
     number = '';
     result = '';
   }
 
-  if(number.length === 15) {
+  // condition prevents adding characters to number, if it is more then 19 characters
+  if(number.length === 20) {
     return false;
   }
 
+  // condition works if we press '.' button, so it inserts '0' before '.'
   if(number.length === 0 && val === '.') {
     //return false;
     number = '0';
   }
 
+  // adding character to number
   if(e.target.classList.contains('number')) {
-    //val = e.target.dataset.value;
+    let val = e.target.dataset.value;
     number += val;
     // console.log(number);
     checkNumberLength();
     currentResult.innerText = number;
-
   } else {
     return false;
   }
@@ -94,43 +90,31 @@ function addNumber(e) {
 }
 
 function addOperator(e) {
-  val = e.target.dataset.value;
+  let val = e.target.dataset.value;
   console.log(number, result, operator);
 
   if(number.length == '0') {
     number = '0';
   }
 
-  // evalDiv.innerText += number;
-  // evalDiv.innerText += operator;
-
-
   if(firstNumber !== '' && operator !== '' && number !== '') {
     evaluate();
     firstNumber = result;
-    //result = 0;
+    // result = 0;
   }
 
   if(result !== '') {
     firstNumber = result;
-    result = '';
+    // result = '';
     number = '';
     operator = val;
-    // evalDiv.innerText += firstNumber;
-    // evalDiv.innerText += operator;
   } else {
     operator = val;
     firstNumber = number;
     number = '';
-    // evalDiv.innerText += number;
-    // evalDiv.innerText += operator;
   }
 
-
-  // evalDiv.innerText += firstNumber + operator;
-
-
-  console.log('fNum:' + firstNumber, 'num:' + number, 'op:' + operator);
+  // console.log('fNum:' + firstNumber, 'num:' + number, 'op:' + operator);
 }
 
 function evaluate() {
@@ -138,8 +122,12 @@ function evaluate() {
     return null;
   }
 
+  if(result === undefined || result == isNaN(NaN) ) {
+    currentResult.innerText = 'Err';
+  }
+
   if(operator === '+') {
-    result = (parseFloat(firstNumber) + parseFloat(number)).toString(10);
+   result = (parseFloat(firstNumber) + parseFloat(number)).toString(10);
     console.log(result);
   } else if(operator === '-') {
     result = (parseFloat(firstNumber) - parseFloat(number)).toString(10);
@@ -148,50 +136,55 @@ function evaluate() {
     result = (parseFloat(firstNumber) * parseFloat(number)).toString(10);
     console.log(result);
   } else if(operator === '/') {
-    result = (parseFloat(firstNumber) / parseFloat(number)).toFixed(10);
+    result = (parseFloat(firstNumber) / parseFloat(number)).toString(10);
     console.log(result);
   }
 
+  if(result.length > 19) {
+    result = 'Err';
+    currentResult.innerText = result;
+    currentResult.style.fontSize = 64 + 'px';
+  }
 
-  currentResult.innerText = result;
   checkNumberLength();
+  currentResult.innerText = result;
   firstNumber = '';
   number = '';
   // result = '';
-
-
 }
 
 function clearLNumb() {
   // console.log(number + 'first console');
-
   number = '';
+
+  if(result !== '0') {
+    result = '';
+    operator = '';
+  }
 
   currentResult.innerText = '0';
   currentResult.style.fontSize = 64 + 'px';
-
-
   // console.log(firstNumber, number + 'second console');
+  if(result === undefined || result == isNaN(NaN) ) {
+    currentResult.innerText = 'Err';
+  }
 }
 
 function cleanAll() {
-  console.log(firstNumber, operator, number, result, 'first console');
+  // console.log(firstNumber, operator, number, result, 'first console');
 
   firstNumber = '';
   operator = '';
   number = '';
   result = '';
-
   currentResult.innerText = '0';
   currentResult.style.fontSize = 64 + 'px';
-  console.log(firstNumber, operator, number, result + 'second console');
+  // console.log(firstNumber, operator, number, result + 'second console');
 }
 
 function delLastSymbol(e) {
   addNumber(e);
-  // let length = number.length;
-  // console.log(number + 'before del');
-
+   // console.log(number + 'before del');
   number = number.substring(0, number.length - 1);
   currentResult.innerText = number;
   checkNumberLength();
@@ -202,55 +195,79 @@ function delLastSymbol(e) {
   }
 }
 
+
 function findSqrt() {
   if(firstNumber !== '') {
     currentResult.innerText = "Err";
     return false;
   }
 
+  if(result !== '') {
+    number = result;
+  }
+
   let num = Math.sqrt(parseInt(number,10));
   number = num.toString();
   console.log(number.length, typeof num);
-  result = number.length > 15 ? parseFloat(num).toFixed(10) : number;
+  result = number.length > 18 ? parseFloat(num).toFixed(15) : number;
 
-  console.log(result);
-
+  // console.log(result);
   currentResult.innerText = result;
   checkNumberLength();
   number = '';
-  result = '';
-}
-
-function addGap() {
-  if(number.length % 3 === 0) {
-    number += 'x';
-  }
+  //result = '';
 }
 
 function evaluateHist(e) {
   let value = e.target.dataset.value;
 
-  evalDiv.innerText += value;
 
-  if(value === '=') {
+  if(value === '«') {
+    let val = evalDiv.innerText;
+    val = val.substring(0, val.length - 1);
+    evalDiv.innerText = '';
+    evalDiv.innerText = val;
+    return false;
+  }
+
+  if(value === 'CE') {
+    evalDiv.innerText = firstNumber + operator;
+    return false;
+  }
+
+  if(result !== '') {
+    evalDiv.innerText = result;
+    evalDiv.innerText += value;
+  } else {
+    evalDiv.innerText += value;
+
+  }
+
+  if(value === '=' || value === 'C' || value === '√') {
     evalDiv.innerText = '';
   }
+
+
+
 }
 
+
+// eventListener for adding numbers
 numbers.forEach(number => number.addEventListener('click', addNumber));
+// eventListener for adding operators
 operators.forEach(operator => operator.addEventListener('click', addOperator));
 
-
+// eventListener for deleting last number
 clearLastNumber.addEventListener('click', clearLNumb);
+// eventListener for clearing all data
 clearAll.addEventListener('click', cleanAll);
+// eventListener for deleting last character
 deleteLastSymbol.addEventListener('click', delLastSymbol);
-
+// eventListener for Math.sqrt()
 sqrt.addEventListener('click', findSqrt);
-
-// display.addEventListener('change', addGap);
-
+// eventListener for agging data to history
 standardButtons.forEach(standardButton => standardButton.addEventListener('click', evaluateHist));
-
+// evaluating data
 equal.addEventListener('click', evaluate);
 
 
